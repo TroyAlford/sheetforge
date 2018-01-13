643dc16f5969f457facc94e46c103b1edd4a9ebf
Troy Alford <troy@troyalford.com>
2018-01-13 14:28:49 -0800

    Refactor Character to use mobx

--- src/models/Character.js
+++ src/models/Character.js
@@ -1,128 +1,37 @@
-import { defaultsDeep, flow, forOwn, pickBy } from 'lodash'
-import autoBind from 'react-autobind'
-import math from '../vendor/mathjs'
+import { computed, observable } from 'mobx'
 
-const DEFAULT = {
-  effects: [],
-  equipment: [],
-  layers: [{
-    attributes: {},
-    effects: [],
-  }],
-  attribute: {
-    max: undefined,
-    min: undefined,
-    value: 0,
-  },
-}
+function sum(a, b) { return a + b }
 
 export default class Character {
-  constructor(
-    /* Character Info */
-    { effects = DEFAULT.effects, equipment = DEFAULT.equipment, layers = DEFAULT.layers } = {},
-    /* Options */
-    { attribute } = {}
-  ) {
-    autoBind(this)
+  layers = observable([]);
+  effects = observable([]);
+  equipment = observable([]);
 
-    this.effects = effects || DEFAULT.effects
-    this.equipment = equipment || DEFAULT.equipment
-    this.layers = layers || DEFAULT.layers
-
-    this.defaults = {
-      attribute: defaultsDeep(attribute, DEFAULT.attribute),
-    }
+  constructor({ layers, effects, equipment } = {}) {
+    this.layers.replace(layers || [])
+    this.effects.replace(effects || [])
+    this.equipment.replace(equipment || [])
   }
 
-  get ActiveEffects() {
+  @computed get modifiers() {
     return [
-      ...this.ActiveLayers.map(layer => layer.effects || []),
-      ...this.equipment.filter(item => item.equipped).map(item => item.effects || []),
-      ...this.effects || [],
+      ...this.layers.filter(layer => !layer.inactive),
+      ...this.effects.filter(effect => effect.active).map(effect => effect.modifiers),
+      ...this.equipment.filter(item => item.equipped).map(item => item.modifiers),
     ]
   }
-  get ActiveLayers() {
-    return this.layers.filter(layer => layer.active !== false)
-  }
 
-  get Attributes() {
-    const attributeDefaults = this.defaults.attribute
-    return this.ActiveLayers
-               .map(layer => layer.attributes)
-               .reduce(this.layerReducer, {})
-  }
+  averageOf = (...names) => Math.round(this.sumOf(...names) / names.length)
+  sumOf = (...names) => names.map(name => this.modifierFor(name)).reduce(sum, 0)
 
-  get Effects() {
-    const all = [
-      ...this.layers.map(layer => layer.effects || {}),
-      ...this.equipment.map(item => item.effects || {}),
-      this.effects,
-    ]
-    // [{ 'key': { val: 1 } }, { 'key': { val: 1 } }]
-
-    const layers = {}
-    all.forEach(layer => Object.keys(layer).map(key => {
-      if (!layers[key]) layers[key] = []
-      layers[key].push(layer[key])
-    }))
-    // { 'key': [{ val: 1 }, { val: 1 }] }
-
-    Object.keys(layers).forEach(key => {
-      layers[key] = layers[key].reduce(this.layerReducer, {})
-    })
-    // [{ 'key': { val: 2 } }]
-
-    return layers
-  }
-
-  get Naked() {
-    return this.ActiveLayers.reduce(this.layerReducer, {})
-  }
-  get Equipped() {
-    return [this.Naked, ...this.ActiveEquipment].reduce(this.layerReducer, {})
-  }
-  get Buffed() {
-    return [this.Equipped, ...this.ActiveEffects].reduce(this.layerReducer, {})
-  }
-
-  calculate(formula, values, options) {
-    const parser = math.parser()
-    const parsed = math.parse(formula)
-    parsed.traverse(node => {
-      if (node.type === 'SymbolNode') {
-        parser.set(node.name,
-          values[node.name] !== undefined ? values[node.name] : options.value
-        )
-      }
-    })
-    let result = parser.eval(formula)
-
-    if (typeof(options.min) === 'number' && result < options.min)
-      result = options.min
-    if (typeof(options.max) === 'number' && result > options.max)
-      result = options.max
-
-    return result
-  }
-  layerReducer(attrs, layer) {
-    const defaults = this.defaults.attribute
-    const numericals = pickBy(layer, value => typeof(value) === 'number')
-    const calculated = pickBy(layer, value => typeof(value) === 'string')
-
-    const changes = {}
-
-    forOwn(numericals, (value, key) => {
-      const current = [attrs[key], defaults.value, 0]
-                      .filter(v => typeof(v) === 'number')[0]
-      attrs[key] = value + current
-    })
-
-    Object.keys(calculated)
-    .map(key => ({ key, calc: calculated[key] }))
-    .forEach(({ key, calc }) => {
-      changes[key] = this.calculate(calc, attrs, defaults)
-    })
-
-    return { ...attrs, ...changes }
-  }
+  modifierFor = name => (
+    this.modifiers
+      .map((modifier) => {
+        if (typeof modifier[name] === 'number') return modifier[name]
+        if (typeof modifier[name] === 'function') return modifier[name].apply(this)
+        return undefined
+      })
+      .filter(value => value !== undefined)
+      .reduce(sum, 0)
+  )
 }
