84cae1d994e9b8a3c8cd95624e557ea65832ad94
Troy Alford <troy@troyalford.com>
2018-01-14 01:39:19 -0800

    Add window-sizing handler, split Attributes/Descriptors out

--- source/components/AxisSheet.js
+++ source/components/AxisSheet.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
 import AxisCharacter from '../models/AxisCharacter'
-import Descriptor from './Descriptor'
+import AxisAttributes from './AxisAttributes'
+import AxisDescriptors from './AxisDescriptors'
+import bound from '../utilities/bound'
 
 import './AxisSheet.scss'
 
@@ -10,60 +12,54 @@ import './AxisSheet.scss'
     super(props)
     this.character = new AxisCharacter()
 
-    const onResize = () => {
-      const portrait = this.container.querySelector('img.portrait')
-      this.setState({
-        portraitHeight: Math.round(portrait.width * (4 / 3)),
-      })
-    }
-    // window.addEventListener('resize', debounce(onResize, 250))
-    window.addEventListener('resize', onResize)
+    window.addEventListener('resize', this.handleWindowResize)
   }
 
   state = {
     portraitHeight: 'auto',
   }
 
+  componentDidMount() {
+    this.handleWindowResize()
+  }
+
+  handleWindowResize = () => {
+    if (!this.container) return
+    const sheetWidth = this.container.offsetWidth
+    const sizeIndex = bound(Math.floor(sheetWidth / 250), { min: 1, max: 3 })
+    const sizeClass = ['small', 'medium', 'large'][sizeIndex - 1]
+
+    const portrait = this.container.querySelector('img.portrait')
+    const portraitHeight = Math.round(portrait.offsetWidth * (4 / 3))
+
+    this.setState({ portraitHeight, sizeClass })
+  }
+
+  handleAttributeChange = (attribute, value) => {
+    this.character[attribute] = value
+  }
   handleDescriptorChange = (descriptor, value) => {
     this.character.descriptors.set(descriptor.toLowerCase(), value)
   }
 
   render = () => {
-    const c = this.character
-    const desc = c.descriptors
+    const { portraitHeight, sizeClass = 'large' } = this.state
+    const { character } = this
 
     return (
-      <div className="sheetforge sheet axis" ref={(self) => { this.container = self }}>
-        <header>{c.name || 'Unnamed Character'}</header>
+      <div className={`sheetforge sheet axis ${sizeClass}`} ref={(self) => { this.container = self }}>
+        <header>{character.name || 'Unnamed Character'}</header>
         <img
           className="portrait"
           alt="Character Portrait"
-          src={c.imageUrl || '//via.placeholder.com/3x4/eeeeee?text=%20'}
+          src={character.imageUrl || '//via.placeholder.com/3x4/eeeeee?text=%20'}
           style={{
-            height: this.state.portraitHeight,
+            height: portraitHeight,
             width: '100%',
           }}
         />
-        <div className="descriptors">
-          <div column-1>
-            <Descriptor name="Homeland" value={desc.get('homeland')} onChange={this.handleDescriptorChange} />
-            <Descriptor name="Race" value={desc.get('race')} onChange={this.handleDescriptorChange} />
-            <Descriptor name="Concept" value={desc.get('concept')} onChange={this.handleDescriptorChange} />
-          </div>
-          <div column-2>
-            <Descriptor name="Height" value={desc.get('height')} onChange={this.handleDescriptorChange} />
-            <Descriptor name="Weight" value={desc.get('weight')} onChange={this.handleDescriptorChange} />
-            <Descriptor name="Gender" value={desc.get('gender')} onChange={this.handleDescriptorChange} />
-          </div>
-          <div column-3>
-            <Descriptor name="Eyes" value={desc.get('eyes')} onChange={this.handleDescriptorChange} />
-            <Descriptor name="Hair" value={desc.get('hair')} onChange={this.handleDescriptorChange} />
-            <Descriptor name="Age" value={desc.get('age')} onChange={this.handleDescriptorChange} />
-          </div>
-        </div>
-        <div className="attributes">
-          <header>Attributes</header>
-        </div>
+        <AxisDescriptors character={character} />
+        <AxisAttributes character={character} />
       </div>
     )
   }
