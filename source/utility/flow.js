d709ad8a24b048845e2afafd2b5984d62182a4c3
Troy Alford <troy@troyalford.com>
2018-01-14 01:45:41 -0800

    Removing extraneous files

--- source/utility/flow.js
+++
@@ -1,5 +0,0 @@
-export const flow = (functions, startWith) => (
-  functions.reduce((value, fn) => fn(value), startWith)
-)
-
-export default flow
--- source/utility/pickBy.js
+++
@@ -1,9 +0,0 @@
-export default (object, filter) => {
-  const hash = {}
-
-  Object.keys(object).forEach((key) => {
-    if (filter(object[key])) hash[key] = object[key]
-  })
-
-  return hash
-}
