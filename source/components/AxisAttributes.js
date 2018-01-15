8df9f52aca75c0a5ba0a62021a1850032e614192
Troy Alford <troy@troyalford.com>
2018-01-14 23:49:36 -0800

    All attributes should use the Attribute component

--- source/components/AxisAttributes.js
+++ source/components/AxisAttributes.js
@@ -7,51 +7,42 @@ export default ({ character }) => (
   <div className="attributes">
     <header>Attributes</header>
 
-    <Attribute name="potency" character={character} computed />
-    <Attribute name="reflex" character={character} computed />
-    <Attribute name="resilience" character={character} computed />
+    <Attribute name="potency" character={character} className="header highlight" computed />
+    <Attribute name="reflex" character={character} className="header highlight" computed />
+    <Attribute name="resilience" character={character} className="header highlight" computed />
 
-    <Attribute name="body" character={character} computed />
+    <Attribute name="body" character={character} className="header highlight" computed />
     <Attribute name="strength" character={character} />
     <Attribute name="agility" character={character} />
     <Attribute name="fitness" character={character} />
 
-    <Attribute name="mind" character={character} computed />
+    <Attribute name="mind" character={character} className="header highlight" computed />
     <Attribute name="intellect" character={character} />
     <Attribute name="acuity" character={character} />
     <Attribute name="focus" character={character} />
 
-    <Attribute name="spirit" character={character} computed />
+    <Attribute name="spirit" character={character} className="header highlight" computed />
     <Attribute name="confidence" character={character} />
     <Attribute name="intuition" character={character} />
     <Attribute name="devotion" character={character} />
 
     <hr className="divider-1" />
 
-    <strong className="attribute" style={{ gridArea: 'racial' }}>Racial:</strong>
+    <Attribute caption="Racial" className="racial header highlight" />
     <Attribute name="size" character={character} />
-    <Attribute name="speed" character={character} />
+    <Attribute name="speed" character={character} computed />
     <Attribute name="naturalArmor" character={character} />
 
-    <strong className="attribute" style={{ gridArea: 'combat' }}>Combat:</strong>
-    <Attribute name="accuracy" character={character} />
-    <Attribute name="might" character={character} />
-    <Attribute name="toughness" character={character} />
+    <Attribute caption="Combat" className="combat header highlight" />
+    <Attribute name="accuracy" character={character} computed />
+    <Attribute name="might" character={character} computed />
+    <Attribute name="toughness" character={character} computed />
 
     <hr className="divider-2" />
 
-    <strong className="attribute" style={{ gridArea: 'thresholds' }}>Thresholds:</strong>
-    <div className="attribute" style={{ gridArea: 'light' }}>
-      <span>Light Wound</span>
-      {character.modifierFor('damageThreshold')}
-    </div>
-    <div className="attribute" style={{ gridArea: 'deep' }}>
-      <span>Deep Wound</span>
-      {character.modifierFor('damageThreshold') * 2}
-    </div>
-    <div className="attribute" style={{ gridArea: 'death' }}>
-      <span>Death Blow</span>
-      {character.modifierFor('damageThreshold') * 4}
-    </div>
+    <Attribute caption="Damage" className="thresholds header highlight" />
+    <Attribute caption="Light Wound" className="light" name="damageThresholdLight" character={character} computed />
+    <Attribute caption="Deep Wound" className="deep" name="damageThresholdDeep" character={character} computed />
+    <Attribute caption="Death Blow" className="death" name="damageThresholdDeath" character={character} computed />
   </div>
 )
