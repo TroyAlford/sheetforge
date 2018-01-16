0ede04c183e0c3d506b2f7cd4137e99687bc8b3d
Troy Alford <troy@troyalford.com>
2018-01-15 16:24:47 -0800

    Improve range() function and add tests

--- source/utilities/range.js
+++ source/utilities/range.js
@@ -1,4 +1,5 @@
-export default (start, stop) => Array.from(
-  new Array((stop - start) + 1),
-  (_, i) => i + start
-)
+export default (start, end) => {
+  const step = (start <= end) ? 1 : -1
+  const length = (Math.abs(end - start) / Math.abs(step)) + 1
+  return Array(...Array(length)).map((_, i) => start + (i * step))
+}
---
+++ source/utilities/range.test.js
@@ -0,0 +1,21 @@
+import range from './range'
+
+jest.unmock('./createRange')
+
+describe('range()', () => {
+  it('returns 1 value when low === high', () => {
+    const expected = [1]
+    const actual = range(1, 1)
+    expect(actual).toEqual(expected)
+  })
+  it('calculates increasing ranges correctly', () => {
+    const expected = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+    const actual = range(-5, 5)
+    expect(actual).toEqual(expected)
+  })
+  it('calculates decreasing ranges correctly', () => {
+    const expected = [5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5]
+    const actual = range(5, -5)
+    expect(actual).toEqual(expected)
+  })
+})
