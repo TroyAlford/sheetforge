a38ebfaf8f03c9602faf881929d546f370801d41
Troy Alford <troy@troyalford.com>
2018-01-15 17:48:06 -0800

    Initial rendering of SkillSection, inc'l dummy data

--- source/AxisRPG/SkillSection.js
+++ source/AxisRPG/SkillSection.js
@@ -3,11 +3,17 @@ import Skill from './Skill'
 
 import './SkillSection.scss'
 
+const SKILLS = [
+  { name: 'Combat: Swordfighting', value: [1, 4] },
+  { name: 'Magic: Arcane', value: [4, 3] },
+]
+
 export default ({ character }) => (
   <div className="skills">
     <header>Skills</header>
-    {character.skills.map(skill =>
-      <Skill character={character} skill={skill} />
-    )}
+    {SKILLS.map((skill) => {
+      console.log(skill)
+      return <Skill character={character} skill={skill} />
+    })}
   </div>
 )
