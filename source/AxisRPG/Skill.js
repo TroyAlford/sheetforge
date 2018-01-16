4e0665b875c28035f3c52935dd8583e5f61ff39d
Troy Alford <troy@troyalford.com>
2018-01-15 17:47:49 -0800

    Add initial rendering for Skill

--- source/AxisRPG/Skill.js
+++ source/AxisRPG/Skill.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react'
+import Editable from '../components/Editable'
 
 export default class Skill extends Component {
+  handleNameChange = () => {
+
+  }
+  handleTheoryChange = () => {
+
+  }
+  handleMasteryChange = () => {
+
+  }
+
   render() {
-    return <div>Foo</div>
+    const { name, value: [theory, mastery] } = this.props.skill
+
+    return (
+      <div className="skill">
+        <Editable className="name" value={name} onChange={this.handleNameChange} />
+        <Editable className="theory" value={theory} onChange={this.handleTheoryChange} />
+        <Editable className="mastery" value={mastery} onChange={this.handleMasteryChange} />
+      </div>
+    )
   }
 }
