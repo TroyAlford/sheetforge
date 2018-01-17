93738399d1d3cc418fc03efd9518c9b860712712
Troy Alford <troy@troyalford.com>
2018-01-16 16:56:27 -0800

    Add a simple hash function for creating unique keys

---
+++ source/utilities/hash.js
@@ -0,0 +1,10 @@
+export default (string = '', radix = 16) => {
+  let h = 0
+  string.split('').forEach((char) => {
+    /* eslint-disable no-bitwise */
+    h = ((h << 5) - h) + char.charCodeAt(0)
+    h &= h // Convert to 32-bit integer
+    /* eslint-enable no-bitwise */
+  })
+  return Math.abs(h).toString(radix)
+}
