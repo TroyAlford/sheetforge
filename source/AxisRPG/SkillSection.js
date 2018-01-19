8cca45d6e860362ee9eff268a8b7a7e5184cdd2e
Troy Alford <troy@troyalford.com>
2018-01-18 19:01:59 -0800

    ESLINT indentation fix

--- source/AxisRPG/SkillSection.js
+++ source/AxisRPG/SkillSection.js
@@ -5,16 +5,16 @@ import Skill from './Skill'
 import './SkillSection.scss'
 
 @observer export default class SkillSection extends Component {
-    render = () => {
-      const { skills = [] } = this.props
-      const rows = Math.ceil(skills.length / 2) + 1
-      const style = { gridTemplateRows: `repeat(${rows}, 1fr)` }
+  render = () => {
+    const { skills = [] } = this.props
+    const rows = Math.ceil(skills.length / 2) + 1
+    const style = { gridTemplateRows: `repeat(${rows}, 1fr)` }
 
-      return (
-        <div className="skills" style={style}>
-          <header>Skills</header>
-          {skills.map(skill => <Skill key={skill.get('id')} skill={skill} />)}
-        </div>
-      )
-    }
+    return (
+      <div className="skills" style={style}>
+        <header>Skills</header>
+        {skills.map(skill => <Skill key={skill.get('id')} skill={skill} />)}
+      </div>
+    )
+  }
 }
