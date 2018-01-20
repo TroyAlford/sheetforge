46b32be576fd872972d5e79d7ddaeda76cc404e1
Troy Alford <troy@troyalford.com>
2018-01-20 13:39:08 -0800

    Improve value getter/setter options and syntax

--- source/models/Armor.js
+++ source/models/Armor.js
@@ -5,22 +5,39 @@ import { average } from '../utilities/math'
 const armorValue = types.refinement(types.number, n => n >= 0)
 const armorArray = types.refinement(types.array(armorValue), a => a.length === 6)
 
+export const REGION = {
+  head: 0,
+  arms: 1,
+  torso: 2,
+  hands: 3,
+  legs: 4,
+  feet: 5,
+}
+
 const Armor = types.model('Armor', {
   id: autoHash,
+  equipped: false,
   name: 'Unnamed Armor',
   values: types.optional(armorArray, [0, 0, 0, 0, 0, 0]),
 }).views(self => ({
-  get head() { return self.values[0] },
-  get arms() { return self.values[1] },
-  get torso() { return self.values[2] },
-  get hands() { return self.values[3] },
-  get legs() { return self.values[4] },
-  get feet() { return self.values[5] },
+  get arms() { return self.values[REGION.arms] },
+  get feet() { return self.values[REGION.feet] },
+  get hands() { return self.values[REGION.hands] },
+  get head() { return self.values[REGION.head] },
+  get legs() { return self.values[REGION.legs] },
+  get torso() { return self.values[REGION.torso] },
 
   get average() { return average(...self.values) },
 })).actions(self => ({
   /* eslint-disable no-param-reassign */
   setName(value) { self.name = value },
+  setArms(value) { self.values[REGION.arms] = value },
+  setFeet(value) { self.values[REGION.feet] = value },
+  setHands(value) { self.values[REGION.hands] = value },
+  setHead(value) { self.values[REGION.head] = value },
+  setLegs(value) { self.values[REGION.legs] = value },
+  setTorso(value) { self.values[REGION.torso] = value },
+  setValue(index, value) { self.values[index] = value },
   setValues(values) { self.values = values },
   /* eslint-enable no-param-reassign */
 }))
--- source/models/Armor.test.js
+++ source/models/Armor.test.js
@@ -1,4 +1,4 @@
-import Armor from './Armor'
+import Armor, { REGION } from './Armor'
 
 it('defaults to 0 for all values', () => {
   const a = Armor.create()
@@ -23,4 +23,18 @@ it('allows setting values', () => {
   expect(a.values.toJS()).toEqual([0, 0, 0, 0, 0, 0])
   a.setValues([1, 2, 3, 4, 5, 6])
   expect(a.values.toJS()).toEqual([1, 2, 3, 4, 5, 6])
+
+  Object.keys(REGION).forEach((region) => {
+    // test .setValue(REGION.head, value) style setter
+    a.setValue(REGION[region], 0)
+    expect(a[region]).toEqual(0)
+
+    // test .setHead(value) style setters
+    const camelCasedRegion = region.replace(/^./, region.charAt(0).toUpperCase())
+    a[`set${camelCasedRegion}`](1)
+    expect(a[region]).toEqual(1)
+  })
+
+  expect(a.values.toJS()).toEqual([1, 1, 1, 1, 1, 1])
+  expect(a.average).toBe(1)
 })
