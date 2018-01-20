0c866b78208287df1c2a4c263d1120cce8300277
Troy Alford <troy@troyalford.com>
2018-01-20 13:45:59 -0800

    Add test for autoHash

---
+++ source/utilities/types.test.js
@@ -0,0 +1,10 @@
+import { types } from 'mobx-state-tree'
+import { autoHash } from './types'
+
+it('automatically creates hash values', () => {
+  const Model = types.model({ id: autoHash })
+  const model = Model.create({})
+
+  expect(typeof model.id).toBe('string')
+  expect(model.id).toMatch(/^[a-f0-9]{7,8}$/)
+})
