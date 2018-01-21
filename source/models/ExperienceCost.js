1c9ad4509e6078181238af864566c4afce782b47
Troy Alford <troy@troyalford.com>
2018-01-21 01:08:27 -0800

    ExperienceCost now guards expenditures, and adjusts XP on update

--- source/models/ExperienceCost.js
+++ source/models/ExperienceCost.js
@@ -1,8 +1,46 @@
-import { types } from 'mobx-state-tree'
+import { clone, getParent, types } from 'mobx-state-tree'
 
-export default function ExperienceCost(fn) {
+export default function ExperienceCost(
+  calcFn,
+  guardedActionNames = [],
+  distanceToCharacter = 2
+) {
   return types.model({
   }).views(self => ({
-    get xpCost() { return fn(self) },
-  }))
+    get xpCost() { return calcFn(self) },
+  })).actions((self) => {
+    const getCharacter = () => (distanceToCharacter ? getParent(self, distanceToCharacter) : self)
+    const getAvailableXP = () => getCharacter().xp
+    const adjustXP = (amount) => {
+      const character = getCharacter()
+      character.setXP(character.xp + amount)
+    }
+
+    const guardedActions = guardedActionNames.reduce((map, name) => ({
+      [`${name}Unguarded`]: self[name],
+      [name]: (...args) => {
+        const availableXP = getAvailableXP()
+
+        const copy = clone(self)
+        copy[`${name}Unguarded`](...args)
+
+        const xpDelta = copy.xpCost - self.xpCost
+        if (availableXP < xpDelta) return undefined
+        adjustXP(-xpDelta)
+        return self[`${name}Unguarded`](...args)
+      },
+    }), {})
+
+    return {
+      ...guardedActions,
+      xpCostAfter(xpFn) {
+        const cloned = clone(self)
+        xpFn(cloned)
+        return cloned.xpCost
+      },
+      xpCostDelta(xpFn) {
+        return self.xpCostAfter(xpFn) - self.xpCost
+      },
+    }
+  })
 }
