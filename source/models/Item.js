0ca4730bba5502318948677499af342b687cbd77
Troy Alford <troy@troyalford.com>
2018-01-20 17:41:13 -0800

    Add Item abstraction

---
+++ source/models/Item.js
@@ -0,0 +1,17 @@
+import { types } from 'mobx-state-tree'
+import { autoHash } from '../utilities/types'
+
+const Item = types.model('Item', {
+  id: autoHash,
+  description: '',
+  name: 'New Item',
+  worth: types.optional(types.refinement(types.number, n => n >= 0), 0),
+}).actions(my => ({
+  /* eslint-disable no-param-reassign */
+  setDescription(value) { my.description = value },
+  setName(value) { my.name = value },
+  setWorth(value) { my.worth = value },
+  /* eslint-enable no-param-reassign */
+}))
+
+export default Item
