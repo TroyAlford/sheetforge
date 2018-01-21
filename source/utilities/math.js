2071904b00c0af8cd6d23da28f6af708c1f7ef2b
Troy Alford <troy@troyalford.com>
2018-01-20 20:45:02 -0800

    Allow math functions to take arrays or multiple arguments

--- source/utilities/math.js
+++ source/utilities/math.js
@@ -1,2 +1,7 @@
-export const sum = (...values) => values.reduce((all, one) => all + one, 0)
-export const average = (...values) => Math.round(sum(...values) / values.length)
+const arrayFn = fn => (arg1, ...args) => {
+  if (!args.length && Array.isArray(arg1)) return fn(arg1)
+  return fn([arg1, ...args])
+}
+
+export const sum = arrayFn(values => values.reduce((all, one) => all + one, 0))
+export const average = arrayFn(values => Math.round(sum(...values) / values.length))
--- source/utilities/math.test.js
+++ source/utilities/math.test.js
@@ -1,14 +1,18 @@
 import * as math from './math'
 
-it('sum() adds numbers', () => {
-  expect(math.sum(1, 2)).toBe(3)
-  expect(math.sum(-1, -2)).toBe(-3)
-  expect(math.sum(1, -1)).toBe(0)
-  expect(math.sum(1, 2, 3, 4, 5)).toBe(15)
+describe('sum()', () => {
+  it('adds numbers', () => {
+    expect(math.sum(1, 2)).toBe(3)
+    expect(math.sum(-1, -2)).toBe(-3)
+    expect(math.sum(1, -1)).toBe(0)
+    expect(math.sum(1, 2, 3, 4, 5)).toBe(15)
+    expect(math.sum([1, 2, 3, 4, 5])).toBe(15)
+  })
 })
 
 it('average() finds the rounded average', () => {
   expect(math.average(1, 2, 3)).toBe(2)
   expect(math.average(1, 2)).toBe(2)
+  expect(math.average([1, 2, 3])).toBe(2)
 })
 
