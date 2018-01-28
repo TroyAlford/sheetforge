f3d7f182e27fd72aa4c2c5cc791f9fdfb77beb9e
Troy Alford <troy@troyalford.com>
2018-01-27 18:17:37 -0800

    autoHash should calculate at .create() time, not at type-definition time

--- source/utilities/types.js
+++ source/utilities/types.js
@@ -1,4 +1,4 @@
 import { types } from 'mobx-state-tree'
 import { randomHash } from './hash'
 
-export const autoHash = types.optional(types.identifier(types.string), randomHash())
+export const autoHash = types.optional(types.identifier(types.string), randomHash)
