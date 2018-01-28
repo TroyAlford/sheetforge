cd07074f9cd9ad549c089cfc2c779a95f0147d12
Troy Alford <troy@troyalford.com>
2018-01-28 00:02:58 -0800

    Simplify Skill editing

--- source/components/Skill.js
+++ source/components/Skill.js
@@ -1,57 +1,41 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
 import Editable from '../components/Editable'
+import noop from '../utilities/noop'
 
 @observer export default class Skill extends Component {
-  handleNameChange = (value) => {
-    this.props.skill.set('name', value)
-  }
-  handleTheoryChange = (theory = 0) => {
-    const { skill } = this.props
-    const [, mastery] = skill.get('value').toJS()
-    skill.set('value', [theory, mastery])
-  }
-  handleMasteryChange = (mastery) => {
-    const { skill } = this.props
-    const [theory] = skill.get('value').toJS()
-    skill.set('value', [theory, mastery])
-  }
-  handleNameEditStart = () => {
-    this.props.skill.set('editingName', true)
-  }
-  handleNameEditEnd = () => {
-    this.props.skill.delete('editingName')
+  static defaultProps = {
+    onEditStart: noop,
+    onEditEnd: noop,
   }
 
   render() {
-    const { skill } = this.props
-    const name = skill.get('name')
-    const [theory, mastery] = skill.get('value').toJS()
+    const { editing, onEditStart, onEditEnd, skill } = this.props
 
     return (
       <div className="skill">
         <Editable
           className="name"
-          forceEditMode={skill.get('editingName')}
-          onChange={this.handleNameChange}
-          onEditStart={this.handleNameEditStart}
-          onEditEnd={this.handleNameEditEnd}
-          value={name}
+          forceEditMode={editing}
+          onChange={skill.setName}
+          onEditStart={onEditStart}
+          onEditEnd={onEditEnd}
+          value={skill.name}
         />
         <Editable
           className="theory"
           max={10}
           min={0}
           type="number"
-          onChange={this.handleTheoryChange}
-          value={theory || ''}
+          onChange={skill.setTheory}
+          value={skill.theory || ''}
         />
         <Editable
           className="mastery"
           max={10}
           min={1}
-          onChange={this.handleMasteryChange}
-          value={mastery}
+          onChange={skill.setMastery}
+          value={skill.mastery}
         />
       </div>
     )
