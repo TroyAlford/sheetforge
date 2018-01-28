ea3538bec0fa1ea78d5c12d4b55c2399cab2cbba
Troy Alford <troy@troyalford.com>
2018-01-28 00:03:14 -0800

    Add "Add" button, and sort skill list

--- source/components/SkillSection.js
+++ source/components/SkillSection.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
+import compareBy from '../utilities/compareBy'
 import Skill from './Skill'
 
 import './SkillSection.scss'
 
 @observer export default class SkillSection extends Component {
   render = () => {
-    const { skills = [] } = this.props
+    const { addSkill, skills = [] } = this.props
     const rows = Math.ceil(skills.length / 2) + 1
     const style = { gridTemplateRows: `repeat(${rows}, 1fr)` }
 
     return (
       <div className="skills" style={style}>
-        <header>Skills</header>
-        {skills.map(skill => <Skill key={skill.get('id')} skill={skill} />)}
+        <header>
+          Skills
+          <button className="icon-add" onClick={addSkill} />
+        </header>
+        {skills.sort(compareBy('name')).map(skill =>
+          <Skill key={skill.id} skill={skill} />
+        )}
       </div>
     )
   }
