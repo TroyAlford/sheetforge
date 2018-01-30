21980c6b6610c8f64d99b2ef6809300525b289e2
Troy Alford <troy@troyalford.com>
2018-01-29 21:07:10 -0800

    Add sub-header to Skills section

--- source/components/SkillSection.js
+++ source/components/SkillSection.js
@@ -8,14 +8,26 @@ import './SkillSection.scss'
 @observer export default class SkillSection extends Component {
   render = () => {
     const { addSkill, skills = [] } = this.props
-    const rows = Math.ceil(skills.length / 2) + 1
-    const style = { gridTemplateRows: `repeat(${rows}, 1fr)` }
+    const rows = Math.ceil(skills.length / 2)
+    const style = { gridTemplateRows: `auto auto repeat(${rows}, 1fr)` }
 
     return (
       <div className="skills" style={style}>
         <header>
           Skills
           <button className="icon-add" onClick={addSkill} />
+        </header>
+        <header className="subheader">
+          <div className="skill">
+            <span className="name">Name</span>
+            <abbr className="theory" title="Theory">Th</abbr>
+            <abbr className="mastery" title="Mastery">Ms</abbr>
+          </div>
+          <div className="skill">
+            <span className="name">Name</span>
+            <abbr className="theory" title="Theory">Th</abbr>
+            <abbr className="mastery" title="Mastery">Ms</abbr>
+          </div>
         </header>
         {skills.sort(compareBy('name')).map(skill =>
           <Skill key={skill.id} skill={skill} />
