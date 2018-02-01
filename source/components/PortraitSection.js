68e9e61f17801ecd283f8c79f3eadc272f8de723
Troy Alford <troy@troyalford.com>
2018-01-31 19:47:37 -0800

    Add PortraitSection

---
+++ source/components/PortraitSection.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import { observer } from 'mobx-react'
+import './PortraitSection.scss'
+
+const DEFAULT_PORTRAIT_URL = '//via.placeholder.com/300x400/eeeeee?text=3x4+aspect+ratio'
+
+const buildUrl = url => `url(${url}), url(${DEFAULT_PORTRAIT_URL})`
+const PortraitSection = ({ url }) => (
+  <div className="portrait section">
+    <header>Portrait</header>
+    <div className="image" style={{ backgroundImage: buildUrl(url) }} />
+  </div>
+)
+
+PortraitSection.displayName = 'PortraitSection'
+export default observer(PortraitSection)
