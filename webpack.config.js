3a984f40c02dd6eea1d38cb78f922de2831da75a
Troy Alford <troy@troyalford.com>
2018-02-02 19:21:58 -0800

    Teach webpack to understand importing font files

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
   devtool: SOURCEMAP ? 'inline-source-map' : 'none',
   module: {
     rules: [{
+      test: /\.(woff2?|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000',
+    }, {
       test: /\.s?css$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
     }],
