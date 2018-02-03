3486baefdb1b3d41a4061a37525c1bca7c5a2edc
Troy Alford <troy@troyalford.com>
2018-02-02 16:13:23 -0800

    Create @ alias for <root>/source

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
+/* eslint-disable no-undef */
+const path = require('path')
+
 const PRODUCTION = process.env.NODE_ENV === 'production'
 const SOURCEMAP = !PRODUCTION || process.env.SOURCEMAP
 
 module.exports = {
+  devtool: SOURCEMAP ? 'inline-source-map' : 'none',
   module: {
     rules: [{
       test: /\.s?css$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
     }],
   },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'source'),
+    },
+  },
 }
+/* eslint-enable no-undef */
