0813392a3cc589b452a6db7fdca02f09ef9a24f3
Troy Alford <troy@troyalford.com>
2018-02-02 16:34:40 -0800

    Single URL for portrait image styling

--- source/components/PortraitSection.js
+++ source/components/PortraitSection.js
@@ -5,7 +5,7 @@ import './PortraitSection.scss'
 
 const DEFAULT_PORTRAIT_URL = '//via.placeholder.com/300x400/eeeeee?text=3x4+aspect+ratio'
 
-const buildUrl = url => `url(${url}), url(${DEFAULT_PORTRAIT_URL})`
+const buildUrl = url => `url(${url || DEFAULT_PORTRAIT_URL})`
 const PortraitSection = ({ url }) => (
   <div className="portrait section">
     <header>Portrait</header>
