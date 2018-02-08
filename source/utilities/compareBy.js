3546748428e2aaed6f41571e087eada2a5c750e1
Troy Alford <troy@troyalford.com>
2018-02-07 18:59:19 -0800

    Safer casting of values to String

--- source/utilities/compareBy.js
+++ source/utilities/compareBy.js
@@ -1,6 +1,6 @@
 export default (prop, reversed = false) => (objectA, objectB) => {
-  const a = (objectA instanceof Map ? objectA.get(prop) : objectA[prop]).toString()
-  const b = (objectB instanceof Map ? objectB.get(prop) : objectB[prop]).toString()
+  const a = String(objectA instanceof Map ? objectA.get(prop) : objectA[prop])
+  const b = String(objectB instanceof Map ? objectB.get(prop) : objectB[prop])
 
   if (reversed) return b.localeCompare(a)
   return a.localeCompare(b)
