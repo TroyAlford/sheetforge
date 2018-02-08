92c5c712508fb514ab38efd1d7be8355328c3f9d
Troy Alford <troy@troyalford.com>
2018-02-07 17:18:30 -0800

    Make Card props more explicit

--- source/components/Card.js
+++ source/components/Card.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
+import noop from '@/utilities/noop'
 
 import './Card.scss'
 
-const renderEmpty = () => { }
-
 @observer export default class Card extends Component {
   static defaultProps = {
-    renderCollapsed: renderEmpty,
-    renderExpanded: renderEmpty,
+    renderAlways: noop,
+    renderCollapsed: noop,
+    renderExpanded: noop,
   }
   state = { expanded: false }
 
@@ -16,15 +16,15 @@ const renderEmpty = () => { }
 
   render = () => {
     const { className, contentsClassName } = this.props
+    const { renderAlways, renderCollapsed, renderExpanded } = this.props
     const expandedClassName = this.state.expanded ? 'expanded' : 'collapsed'
 
     return (
       <div className={`card ${expandedClassName} ${className || ''}`.trim()}>
         <div className={`expander icon-${expandedClassName}`} onClick={this.toggleExpanded} />
         <div className={`card-contents ${contentsClassName || ''}`.trim()}>
-          {this.props.children}
-          {this.state.expanded || this.props.renderCollapsed()}
-          {this.state.expanded && this.props.renderExpanded()}
+          {renderAlways()}
+          {this.state.expanded ? renderExpanded() : renderCollapsed()}
         </div>
       </div>
     )
