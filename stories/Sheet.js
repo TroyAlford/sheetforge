d4306cce9b3705e9358d0a45655d917f479d14bd
Troy Alford <troy@troyalford.com>
2018-02-08 18:35:51 -0800

    Add localStorage persistent story

--- stories/Sheet.js
+++ stories/Sheet.js
@@ -2,7 +2,19 @@ import React from 'react'
 import { storiesOf } from '@storybook/react' // eslint-disable-line
 import Sheet from '../source/components/Sheet'
 
+function saveToLocalStorage(object) {
+  localStorage.setItem('stories/Sheet', JSON.stringify(object))
+}
+function loadFromLocalStorage() {
+  const json = localStorage.getItem('stories/Sheet') || '{}'
+  const character = JSON.parse(json)
+  return character
+}
+
 storiesOf('Sheet', module)
   .add('basic', () => (
     <Sheet />
+  ))
+  .add('persist to localStorage', () => (
+    <Sheet character={loadFromLocalStorage()} onChange={saveToLocalStorage} />
   ))
