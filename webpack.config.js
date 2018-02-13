e2b5e962789a43838d58d60647ab781abeb42149
Troy Alford <troy@troyalford.com>
2018-02-13 00:59:54 -0800

    Fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,33 @@
 /* eslint-disable no-undef */
 const path = require('path')
 
-const PRODUCTION = process.env.NODE_ENV === 'production'
+const ENVIRONMENT = process.env.NODE_ENV
+const PRODUCTION = ENVIRONMENT === 'production'
 const SOURCEMAP = !PRODUCTION || process.env.SOURCEMAP
+
+const plugins = []
+
+if (PRODUCTION) {
+  plugins.push(
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(ENVIRONMENT) }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      output: { comments: false, semicolons: false },
+      sourceMap: SOURCEMAP,
+    })
+  )
+}
 
 module.exports = {
   devtool: SOURCEMAP ? 'inline-source-map' : 'none',
+  entry: `${__dirname}/source/components/Sheet.js`,
   module: {
     rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }, {
       test: /\.(woff2?|eot|ttf|svg)$/,
       loader: 'url-loader?limit=100000',
     }, {
@@ -15,11 +35,19 @@ module.exports = {
       use: ['style-loader', 'css-loader', 'sass-loader'],
     }],
   },
+  output: {
+    filename: PRODUCTION ? 'sheetforge.min.js' : 'sheetforge.js',
+    library: 'sheetforge',
+    libraryTarget: 'umd',
+    path: `${__dirname}/build`,
+    umdNamedDefine: true,
+  },
   resolve: {
     extensions: ['.js'],
     alias: {
-      '@': path.resolve(__dirname, 'source'),
+      '@': path.resolve(__dirname, 'source/'),
     },
   },
+  plugins,
 }
 /* eslint-enable no-undef */
