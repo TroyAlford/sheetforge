6f95c190d7592ae2ea75671ec99962482af152d5
Troy Alford <troy@troyalford.com>
2018-07-21 15:26:35 -0700

    Update docs & build scripts

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,23 @@
+const webpack = require('webpack')
 const config = require('../webpack.config')
-config.externals = {}
 
-module.exports = config
+module.exports = {
+  ...config,
+  externals: {},
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }, {
+      test: /\.(woff2?|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000',
+    }, {
+      test: /\.s?css$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    }],
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+  ]
+}
