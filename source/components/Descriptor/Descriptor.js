582b63ab5896ca7a26b6f152ab98e2420cf3ca91
Troy Alford <troy@troyalford.com>
2018-09-24 17:04:48 -0700

    Descriptor editor

---
+++ source/components/Descriptor/Descriptor.js
@@ -0,0 +1,31 @@
+import { observer } from 'mobx-react'
+import React, { Component } from 'react'
+import Editable from '@/components/Editable'
+import './Descriptor.scss'
+
+export default @observer class Descriptor extends Component {
+  static defaultProps = {
+    model: {},
+  }
+
+  onChangeName = displayName => this.props.model.set({ displayName })
+
+  onChangeValue = value => this.props.model.set({ value })
+
+  render() {
+    return (
+      <div className="descriptor">
+        <Editable
+          className="displayName"
+          onChange={this.onChangeName}
+          value={this.props.model.displayName}
+        />
+        <Editable
+          className="value"
+          onChange={this.onChangeValue}
+          value={this.props.model.value}
+        />
+      </div>
+    )
+  }
+}
