d9e321d3b7ef8ebb2eda0e9531d13b7845b3738e
Troy Alford <troy@troyalford.com>
2018-09-30 00:15:04 -0700

    Add Collection model

---
+++ source/models/generic/Collection/Collection.js
@@ -0,0 +1,52 @@
+import { isObservableArray } from 'mobx'
+import { types } from 'mobx-state-tree'
+
+export default (TYPE, options = {}) => {
+  const TypedArrayWrapper = types.model({
+    values: types.array(TYPE),
+  }).views(self => ({
+    get asArray() { return [...self.values] },
+    get length() { return self.values.length },
+  })).actions(self => ({
+    /* eslint-disable no-param-reassign */
+
+    at: n => self.values[n],
+    clear: () => { self.values = [] },
+    delete: (object) => { self.values = self.values.filter(item => item !== object) },
+    deleteAt: (index) => { self.values = self.values.filter((_, n) => n !== index) },
+    every: fn => self.asArray.every(fn),
+    filter: fn => self.asArray.filter(fn),
+    find: fn => self.asArray.find(fn),
+    forEach: fn => self.asArray.forEach(fn),
+    insert: (object, index) => { self.splice(index, 0, object) },
+    map: fn => self.asArray.map(fn),
+    pop: () => self.values.pop(),
+    push: (...values) => self.values.push(...values),
+    reduce: (fn, initial) => self.values.reduce(fn, initial),
+    shift: () => self.values.shift(),
+    slice: (start, end) => self.values.slice(start, end),
+    some: fn => self.asArray.some(fn),
+    splice: (index, deleteCount, value) => { self.values.splice(index, deleteCount, value) },
+    unshift: (...values) => self.values.unshift(...values),
+
+    /* eslint-enable no-param-reassign */
+  }))
+
+  return types.custom({
+    name: options.name || 'Collection', // eslint-disable-next-line sort-keys
+    fromSnapshot: (snapshot = []) => TypedArrayWrapper.create({ values: snapshot }),
+    getValidationMessage: (snapshot) => {
+      if (!snapshot) return ''
+      if (isObservableArray(snapshot) || Array.isArray(snapshot)) {
+        if (snapshot.every(f => TYPE.is(f))) return ''
+      }
+
+      return 'Collection types must be assigned an array of values or null'
+    },
+    isTargetType: target => (
+      target && target.values && isObservableArray(target.values) && target.values.every(TYPE.is)
+    ),
+    toSnapshot: value => (value ? value.values.toJSON() : []),
+    ...options,
+  })
+}
---
+++ source/models/generic/Collection/Collection.test.js
@@ -0,0 +1,135 @@
+import { types } from 'mobx-state-tree'
+import Collection from './Collection'
+
+const Foo = types.model('Foo', {
+  value: types.string,
+})
+const MyCollection = Collection(Foo)
+
+describe('models/Collection', () => {
+  let collection
+
+  beforeEach(() => {
+    collection = MyCollection.create([
+      { value: 'Foo!' },
+      { value: 'Bar!' },
+      { value: 'Baz!' },
+      { value: 'Qux!' },
+    ])
+  })
+
+  it('can serialize/deserialize', () => {
+    expect(collection).toHaveLength(4)
+    expect(collection.toJSON()).toMatchSnapshot()
+  })
+
+  it('supports at(n)', () => {
+    collection.forEach((item, n) => {
+      expect(collection.at(n)).toBe(item)
+    })
+  })
+
+  it('supports clear()', () => {
+    expect(collection).toHaveLength(4)
+    expect(collection.clear())
+    expect(collection).toHaveLength(0)
+  })
+
+  it('supports delete(item)', () => {
+    expect(collection).toHaveLength(4)
+    collection.delete(collection.at(2))
+    expect(collection).toHaveLength(3)
+    expect(collection).toMatchSnapshot()
+  })
+
+  it('supports deleteAt(item)', () => {
+    expect(collection).toHaveLength(4)
+    collection.deleteAt(2)
+    expect(collection).toHaveLength(3)
+    expect(collection).toMatchSnapshot()
+  })
+
+  it('supports every(fn)', () => {
+    expect(collection.every(item => item.value.includes('!'))).toEqual(true)
+    expect(collection.every(item => (item.value === 'Foo!'))).toEqual(false)
+  })
+
+  it('supports filter(fn)', () => {
+    expect(collection.filter(({ value }) => value.match(/a/)))
+      .toEqual([{ value: 'Bar!' }, { value: 'Baz!' }])
+  })
+
+  it('supports find(fn)', () => {
+    expect(collection.find(({ value }) => value.match(/^F/)).toJSON())
+      .toEqual({ value: 'Foo!' })
+  })
+
+  it('supports forEach(fn)', () => {
+    collection.forEach(({ value }) => {
+      expect(value).toBeTruthy()
+    })
+  })
+
+  it('supports insert(object, index)', () => {
+    collection.insert({ value: 'Inserted!' }, 2)
+    expect(collection.map(({ value }) => value)).toEqual(['Foo!', 'Bar!', 'Inserted!', 'Baz!', 'Qux!'])
+    collection.insert({ value: 'First!' })
+    expect(collection.map(({ value }) => value)).toEqual(['First!', 'Foo!', 'Bar!', 'Inserted!', 'Baz!', 'Qux!'])
+  })
+
+  it('supports map(fn)', () => {
+    const result = collection.map((item) => {
+      expect(item).toBeTruthy()
+      return item.toJSON()
+    })
+    expect(result).toMatchSnapshot()
+  })
+
+  it('supports pop()', () => {
+    expect(collection).toHaveLength(4)
+    expect(collection.pop().toJSON()).toEqual({ value: 'Qux!' })
+    expect(collection).toHaveLength(3)
+  })
+
+  it('supports push()', () => {
+    expect(collection).toHaveLength(4)
+    collection.push({ value: 'Yay!' })
+    expect(collection).toHaveLength(5)
+    expect(collection.at(4).toJSON()).toEqual({ value: 'Yay!' })
+  })
+
+  it('supports reduce(fn, initial)', () => {
+    const reduced = collection.reduce((all, item) => (
+      [all, item.value].filter(Boolean).join('-')
+    ), '')
+    expect(reduced).toEqual('Foo!-Bar!-Baz!-Qux!')
+  })
+
+  it('supports shift()', () => {
+    expect(collection).toHaveLength(4)
+    expect(collection.shift().toJSON()).toEqual({ value: 'Foo!' })
+    expect(collection).toHaveLength(3)
+  })
+
+  it('supports slice(start, end)', () => {
+    expect(collection.slice(2)).toEqual([{ value: 'Baz!' }, { value: 'Qux!' }])
+    expect(collection.slice(0, 1)).toEqual([{ value: 'Foo!' }])
+  })
+
+  it('supports some(fn)', () => {
+    expect(collection.some(({ value }) => value === 'Baz!')).toEqual(true)
+    expect(collection.some(({ value }) => value === 'Random!')).toEqual(false)
+  })
+
+  it('supports splice(index, deleteCount, value)', () => {
+    collection.splice(1, 2, { value: 'Inserted!' })
+    expect(collection.map(({ value }) => value)).toEqual(['Foo!', 'Inserted!', 'Qux!'])
+  })
+
+  it('supports unshift()', () => {
+    expect(collection).toHaveLength(4)
+    collection.unshift({ value: 'Yay!' })
+    expect(collection).toHaveLength(5)
+    expect(collection.at(0).toJSON()).toEqual({ value: 'Yay!' })
+  })
+})
