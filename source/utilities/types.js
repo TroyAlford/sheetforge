1574bb959a5dbcd51ae735a9f23738360eb46f0f
Troy Alford <troy@troyalford.com>
2018-10-06 20:22:39 -0700

    Remove deprecated function

--- source/utilities/types.js
+++
@@ -1,5 +0,0 @@
-import { types } from 'mobx-state-tree'
-import { randomHash } from '@/utilities/hash'
-
-// eslint-disable-next-line import/prefer-default-export
-export const autoHash = types.optional(types.identifier, randomHash)
