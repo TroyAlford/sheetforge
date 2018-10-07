18fd51555be072f616d075de88f1c2e2dd784157
Troy Alford <troy@troyalford.com>
2018-10-06 20:04:22 -0700

    Extract Expandable

--- source/components/Item/Item.js
+++ source/components/Item/Item.js
@@ -2,6 +2,7 @@ import { observer } from 'mobx-react'
 import React, { Component } from 'react'
 import Editable from '@/components/Editable'
 import EffectList from '@/components/EffectList'
+import Expandable from '@/components/Expandable'
 import ItemModel from '@/models/Item'
 import modelPropType from '@/utilities/prop-types/model'
 import './Item.scss'
@@ -15,29 +16,20 @@ import './Item.scss'
     model: modelPropType(ItemModel),
   }
 
-  state = {
-    expanded: false,
-  }
-
   handleChangeDescription = description => this.props.model.set({ description })
 
   handleChangeName = displayName => this.props.model.set({ displayName })
 
   handleToggleEquipped = () => this.props.model.set({ equipped: !this.props.model.equipped })
 
-  // eslint-disable-next-line react/no-access-state-in-setstate
-  handleToggleExpanded = () => this.setState({ expanded: !this.state.expanded })
-
   render() {
     const { description, displayName, effects, equipped } = this.props.model
-    const toggleState = this.state.expanded ? 'expanded' : 'collapsed'
 
     return (
       <div className={`item ${equipped ? '' : 'un'}equipped`}>
         <Editable className="equipped" onChange={this.handleToggleEquipped} value={equipped} />
         <Editable className="displayName" onChange={this.handleChangeName} value={displayName} />
-        <div className={`expanded-toggle icon-${toggleState}`} onClick={this.handleToggleExpanded} />
-        {this.state.expanded && <>
+        <Expandable>
           <Editable
             className="description"
             type="multiline"
@@ -45,8 +37,8 @@ import './Item.scss'
             placeholder="Description..."
             value={description}
           />
-          <EffectList expanded model={effects} />
-        </>}
+          <EffectList collection={effects} />
+        </Expandable>
       </div>
     )
   }
