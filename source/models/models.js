f75be9aebd98344b4a2bcfb437f607de0c3a2224
Troy Alford <troy@troyalford.com>
2018-10-17 16:52:42 -0700

    Export components & models

---
+++ source/models/models.js
@@ -0,0 +1,21 @@
+import Attribute from '@/models/Attribute'
+import Descriptor from '@/models/Descriptor'
+import Effect from '@/models/Effect'
+import HealthLevel from '@/models/HealthLevel'
+import Item from '@/models/Item'
+import Resource from '@/models/Resource'
+import Skill from '@/models/Skill'
+import Spell from '@/models/Spell'
+import Trait from '@/models/Trait'
+
+export default {
+  Attribute,
+  Descriptor,
+  Effect,
+  HealthLevel,
+  Item,
+  Resource,
+  Skill,
+  Spell,
+  Trait,
+}
