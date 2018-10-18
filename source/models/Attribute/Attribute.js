2a1347bbfe48e7d5128303add500afe98a759720
Troy Alford <troy@troyalford.com>
2018-10-17 17:08:33 -0700

    Improve consistency with other models

--- source/models/Attribute/Attribute.js
+++ source/models/Attribute/Attribute.js
@@ -8,22 +8,22 @@ export default types.compose(
   types.model({
     name: types.identifier,
     value: 0,
-  }).views((self) => {
-    let character
-
-    return {
-      afterAttach() {
-        try { character = findParent(self, p => p.isCharacter) } catch (e) {}
-      },
-      get displayValue() { return self.value + self.modifier },
-      get effects() {
-        if (!character) return []
-        return character.activeEffects.filter(effect => effect.target === self)
-      },
-      get modifier() { return sum(self.effects.map(effect => effect.modifier)) },
-      get modifierText() {
-        return self.effects.map(({ modifier, sourceName }) => `${sourceName}: ${modifier}`).join(', ')
-      },
-    }
-  })
+  }).volatile(() => ({
+    character: null,
+  })).views(self => ({
+    get displayValue() { return self.value + self.modifier },
+    get effects() {
+      if (!self.character) return []
+      return self.character.activeEffects.filter(effect => effect.target === self)
+    },
+    get modifier() { return sum(self.effects.map(effect => effect.modifier)) },
+    get modifierText() {
+      return self.effects.map(({ modifier, sourceName }) => `${sourceName}: ${modifier}`).join(', ')
+    },
+  })).actions(self => ({
+    afterAttach() {
+      // eslint-disable-next-line no-param-reassign
+      try { self.character = findParent(self, p => p.isCharacter) } catch (e) { }
+    },
+  }))
 ).named('Attribute')
