0b2b15919076dc7422e5e5aa802aea57d5929312
Troy Alford <troy@troyalford.com>
2018-10-17 17:05:54 -0700

    Improve layout components

--- source/components/Sheet/Section/Section.js
+++ source/components/Sheet/Section/Section.js
@@ -1,4 +1,9 @@
 import React from 'react'
 import './Section.scss'
 
-export default ({ children }) => <div className="section">{children}</div>
+export default ({ children, flex = 1, title }) => (
+  <div className="section" style={{ flex }}>
+    {title && <h1>{title}</h1>}
+    {children}
+  </div>
+)
