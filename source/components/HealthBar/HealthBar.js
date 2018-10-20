72f730940c97a9c858005e34436c05403dada084
Troy Alford <troy@troyalford.com>
2018-10-19 21:25:50 -0700

    Clean up HealthBar

--- source/components/HealthBar/HealthBar.js
+++ source/components/HealthBar/HealthBar.js
@@ -15,48 +15,36 @@ const HealthLevelList = ListOf(HealthLevelModel, HealthLevel, {
 
 @observer class HealthBar extends Component {
   static defaultProps = {
-    horizontal: true,
     model: [],
+    title: '',
     vertical: false,
   }
 
   static propTypes = {
-    horizontal: PropTypes.bool,
     model: modelPropType(CollectionOf(HealthLevelModel)),
+    title: PropTypes.string,
     vertical: PropTypes.bool,
   }
 
-  handleHeal = () => {
-    const injury = [...this.props.model.slice().reverse()].find(level => level.damage !== 'none')
-    if (injury) injury.apply('none')
-  }
-
-  handleHarm = () => {
-    const healthy = [...this.props.model.asArray].find(level => level.damage === 'none')
-    if (healthy) healthy.apply('heavy')
-  }
-
   render() {
-    const { horizontal, model, vertical } = this.props
+    const { model, title, vertical } = this.props
     const current = model.filter(level => level.damage === 'none').length
     const worstInjury = [...model.slice().reverse()].find(level => level.damage !== 'none')
     const classNames = [
       'health-bar',
-      horizontal && !vertical && 'horizontal',
-      vertical && 'vertical',
+      vertical ? 'vertical' : 'horizontal',
     ].filter(Boolean).join(' ')
+
+    const titleText = <>
+      {title || 'Health'} {current}/{model.length}
+      {Boolean(worstInjury) && (
+        <div className="wound-level">({worstInjury.name}: {worstInjury.penalty})</div>
+      )}
+    </>
 
     return (
       <div className={classNames}>
-        <div className="summary">
-          <b>Health</b>:&nbsp;{current} of {model.length} {worstInjury && `(${worstInjury.name})`}
-        </div>
-        {worstInjury && <div className="penalty">Penalty: {worstInjury.penalty}</div>}
-        <button onClick={this.handleHarm}> - </button>
-        <button onClick={this.handleHeal}> + </button>
-        <Expandable>
-          <HealthLevelList collection={model} />
-        </Expandable>
+        <HealthLevelList collection={model} title={titleText} />
       </div>
     )
   }
