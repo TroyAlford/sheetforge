14ed1bdf73660c91f3270caa98cdd0be99020762
Troy Alford <troy@troyalford.com>
2018-10-21 23:33:56 -0700

    Improving expandability and layout

--- source/components/Expandable/Expandable.js
+++ source/components/Expandable/Expandable.js
@@ -6,16 +6,30 @@ export default class Expandable extends Component {
     expanded: false,
   }
 
-  // eslint-disable-next-line react/no-access-state-in-setstate
-  handleToggle = () => this.setState({ expanded: !this.state.expanded })
+  expander = React.createRef()
+
+  handleToggle = () => {
+    const { classList } = this.expander.current.parentElement
+
+    // eslint-disable-next-line react/no-access-state-in-setstate
+    this.setState({ expanded: !this.state.expanded }, () => {
+      classList.add(this.state.expanded ? 'expandable-wrapper-expanded' : 'expandable-wrapper-collapsed')
+      classList.remove(this.state.expanded ? 'expandable-wrapper-collapsed' : 'expandable-wrapper-expanded')
+    })
+  }
 
   render() {
     const { children } = this.props
     const toggleState = this.state.expanded ? 'expanded' : 'collapsed'
+    const buttonProps = {
+      className: `expandable-toggle icon-${toggleState}`,
+      onClick: this.handleToggle,
+      ref: this.expander,
+    }
 
     return (
       <Fragment>
-        <div className={`expandable-toggle icon-${toggleState}`} onClick={this.handleToggle} />
+        <div {...buttonProps} />
         {this.state.expanded && <div className="expandable">{children}</div>}
       </Fragment>
     )
