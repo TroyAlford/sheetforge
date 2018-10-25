b4002dd031bd2ffe0f10dd4b4f238d6100c42cb8
Troy Alford <troy@troyalford.com>
2018-10-25 16:39:12 -0700

    Add allowExcess, with configurable icon

--- source/components/Rating/Rating.js
+++ source/components/Rating/Rating.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
+import math from '@/utilities/math'
 import noop from '@/utilities/noop'
 import range from '@/utilities/range'
 import './Rating.scss'
 
 export default class Rating extends Component {
   static defaultProps = {
+    allowExcess: false,
     current: 0,
-    iconOff: <span className="dot" />,
+    iconExcess: <span className="dot excess icon-add" />,
+    iconOff: <span className="dot empty" />,
     iconOn: <span className="dot filled" />,
     maximum: 5,
     onChange: noop,
@@ -18,12 +21,16 @@ export default class Rating extends Component {
   }
 
   render = () => {
-    const { current, iconOff, iconOn, maximum } = this.props
+    const { allowExcess, current, iconExcess, iconOff, iconOn, maximum } = this.props
+    const total = allowExcess ? math.max(current, maximum) : maximum
+
     return (
       <div className="rating">
-        {range(1, maximum).map(number => (
-          <span key={number} data-number={number} onClick={this.handleIconClick} title={number}>
-            {number <= current ? iconOn : iconOff}
+        {range(1, total).map(number => (
+          <span data-number={number} key={number} onClick={this.handleIconClick} title={number}>
+            {(number <= current && number <= maximum) && iconOn}
+            {(number > current && number <= maximum) && iconOff}
+            {(number > maximum) && iconExcess}
           </span>
         ))}
       </div>
