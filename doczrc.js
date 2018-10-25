13ed69cb526a29ed9d05baf7aa5852f9cb240085
Troy Alford <troy@troyalford.com>
2018-10-24 22:56:02 -0700

    Update documentation bundle

--- doczrc.js
+++ doczrc.js
@@ -2,19 +2,21 @@ import merge from 'webpack-merge' // eslint-disable-line
 import webpackConfig from './webpack.config.babel'
 
 export default {
+  base: '/sheetforge/',
   dest: './docs',
   head: {
     meta: [{ content: 'width=device-width, initial-scale=1', name: 'viewport' }],
   },
   modifyBundlerConfig: config => merge(config, {
     ...webpackConfig,
+    externals: {},
     output: {
       filename: 'sheetforge.[name].min.js',
       library: 'sheetforge',
       libraryTarget: 'umd',
       path: `${__dirname}/docs`, // eslint-disable-line no-undef
       umdNamedDefine: true,
-    }
+    },
   }),
   src: './source',
   themeConfig: {
