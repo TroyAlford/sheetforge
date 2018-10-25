cdf3a16a7582e18160dce6ac01dcdff44c05b142
Troy Alford <troy@troyalford.com>
2018-10-24 17:26:58 -0700

    Improving build scripts

--- doczrc.js
+++ doczrc.js
@@ -1,15 +1,12 @@
 import merge from 'webpack-merge' // eslint-disable-line
-import webpackConfig from './webpack.config'
+import webpackConfig from './webpack.config.babel'
 
 export default {
   dest: './docs',
   head: {
     meta: [{ content: 'width=device-width, initial-scale=1', name: 'viewport' }],
   },
-  modifyBundlerConfig: config => merge(
-    config,
-    webpackConfig,
-  ),
+  modifyBundlerConfig: config => merge(config, webpackConfig),
   src: './source',
   themeConfig: {
     styles: {
