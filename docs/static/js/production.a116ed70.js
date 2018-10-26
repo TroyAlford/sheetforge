ef07cf125c8e68c818531bfe54c814073eb02ce4
Troy Alford <troy@troyalford.com>
2018-10-25 22:46:20 -0700

    Updating documentation

--- docs/static/js/production.a116ed70.js
+++ docs/static/js/production.a116ed70.js
@@ -1,2 +1,2 @@
 !function(e,o){"object"===typeof exports&&"object"===typeof module?module.exports=o():"function"===typeof define&&define.amd?define("sheetforge",[],o):"object"===typeof exports?exports.sheetforge=o():e.sheetforge=o()}(window,function(){return(window.webpackJsonpsheetforge=window.webpackJsonpsheetforge||[]).push([[2],{"05fm":function(e,o,t){"use strict";t.r(o);var f=t("ITKj");t("H3yI");o.default=f.a},H3yI:function(e,o,t){}},[["05fm",16,0]]])});
-//# sourceMappingURL=production.4b0549fcbdca0cd71010.js.map
+//# sourceMappingURL=production.2ca8fd44eb9aa0074cc9.js.map
