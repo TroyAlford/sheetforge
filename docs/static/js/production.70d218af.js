cfbb868795f3346f38bd5b30db72d7dac96b3ddc
Troy Alford <troy@troyalford.com>
2018-11-04 02:23:14 -0800

    v2.1.0

--- docs/static/js/production.70d218af.js
+++
@@ -1,2 +0,0 @@
-!function(e,o){"object"===typeof exports&&"object"===typeof module?module.exports=o():"function"===typeof define&&define.amd?define("sheetforge",[],o):"object"===typeof exports?exports.sheetforge=o():e.sheetforge=o()}(window,function(){return(window.webpackJsonpsheetforge=window.webpackJsonpsheetforge||[]).push([[15],{"05fm":function(e,o,t){"use strict";t.r(o);var f=t("ITKj");t("H3yI");o.default=f.a},H3yI:function(e,o,t){}},[["05fm",17,0]]])});
-//# sourceMappingURL=production.65911198011775802943.js.map
