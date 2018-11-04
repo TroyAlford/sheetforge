6fa07bd221511c59bf6355523743acb25efbc1a2
Troy Alford <troy@troyalford.com>
2018-11-04 02:22:10 -0800

    Cleanup

--- source/models/Layout/Layout.js
+++ source/models/Layout/Layout.js
@@ -20,39 +20,3 @@ const Layout = types.compose(
 ).named('Layout')
 
 export default Layout
-
-// RULES
-// - Anything INSIDE a section that is NOT a section is 100% width
-// - A section which has a colSpan passes it down as `columns` to its children
-// - A section with a `list` property assumes component/model name match
-
-// [
-//   { type: 'Section', title: 'Descriptors', children: [
-//     { type: 'Descriptor', list: true, path: 'descriptors' },
-//   ] },
-//   { type: 'Section', title: 'Attributes', children: [
-//     { type: 'Attribute', list: true, path: 'attributes' },
-//     { type: 'HealthBar', path: 'health' },
-//   ] },
-//   { type: 'Row', children: [
-//     { type: 'Column', flex: 1, children: [
-//       { type: 'Section', title: 'Traits', children: [
-//         { type: 'Trait', list: true, path: 'traits' },
-//       ] },
-//       { type: 'Section', title: 'Resources', children: [
-//         { type: 'Resource', list: true, path: 'resources' },
-//       ] },
-//     ] },
-//     { type: 'Column', flex: 2, title: 'Skills', children: [
-//       { type: 'Section', title: 'Skills', children: [
-//         { type: 'Skill', list: true, path: 'skills' },
-//       ] },
-//       { type: 'Section', title: 'Inventory', children: [
-//         { type: 'Item', list: true, path: 'items' },
-//       ] },
-//       { type: 'Section', title: 'Spellbook', children: [
-//         { type: 'Spell', list: true, path: 'spells' },
-//       ] }
-//     ] },
-//   ] }
-// ]
