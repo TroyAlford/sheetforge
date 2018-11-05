6edb9a1665f3b740496c405ef4e19b48c2f1b758
Troy Alford <troy@troyalford.com>
2018-11-04 22:59:17 -0800

    Prevent circular reference calculations from going infinite

--- source/models/Attribute/Attribute.js
+++ source/models/Attribute/Attribute.js
@@ -22,36 +22,46 @@ export default types.compose(
         .replace(/[^a-z0-9]/ig, '')
     },
     get isComputed() { return typeof self.raw === 'string' },
-  })).actions(self => ({
-    effects() {
-      if (!self.character) return []
-      return self.character.activeEffects().filter(effect => effect.targetId === self.id)
-    },
-    modifiedValue() { return (self.value() || 0) + self.modifier() },
-    modifier() { return math.sum(self.effects().map(effect => effect.modifier)) },
-    modifierText() {
-      return self.effects().map(effect => `${effect.sourceName()}: ${effect.modifier}`).join(', ')
-    },
-    value() {
-      if (!self.isComputed) return self.raw
-      if (self.character === null) return 0
+  })).actions((self) => {
+    let computing = false
 
-      try {
-        const values = toSymbols(self.raw).reduce((all, symbol) => {
-          const attribute = self.character.attributes.findBy('id', symbol)
-          return Object.assign(all, { [symbol]: attribute ? attribute.modifiedValue() : 0 })
-        }, {})
+    return ({
+      effects() {
+        if (!self.character) return []
+        return self.character.activeEffects().filter(effect => effect.targetId === self.id)
+      },
+      modifiedValue() { return (self.value() || 0) + self.modifier() },
+      modifier() { return math.sum(self.effects().map(effect => effect.modifier)) },
+      modifierText() {
+        return self.effects().map(effect => `${effect.sourceName()}: ${effect.modifier}`).join(', ')
+      },
+      value() {
+        if (!self.isComputed) return self.raw
+        if (self.character === null) return 0
+        if (computing) return 0
 
-        return calculate(self.raw, values)
-      } catch (error) {
-        // eslint-disable-next-line no-undef, no-console
-        if (process.env.NODE_ENV !== 'production') {
-          console.error(`Error computing: ${self.raw}`)
+        computing = true
+        let computed = 0
+
+        try {
+          const values = toSymbols(self.raw).reduce((all, symbol) => {
+            const attribute = self.character.attributes.findBy('id', symbol)
+            return Object.assign(all, { [symbol]: attribute ? attribute.modifiedValue() : 0 })
+          }, {})
+
+          computed = calculate(self.raw, values)
+        } catch (error) {
+          // eslint-disable-next-line no-undef, no-console
+          if (process.env.NODE_ENV !== 'production') {
+            console.error(`Error computing: ${self.raw}`)
+          }
+          computed = 0
         }
-        return 0
-      }
-    },
-  }))
-    .preProcessSnapshot(({ value, ...snapshot }) => ({ ...snapshot, raw: value }))
+
+        computing = false
+        return computed
+      },
+    })
+  }).preProcessSnapshot(({ value, ...snapshot }) => ({ ...snapshot, raw: value }))
     .postProcessSnapshot(({ raw, ...snapshot }) => ({ ...snapshot, value: raw }))
 ).named('Attribute')
